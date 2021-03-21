f326b943643c5569e389261fe2268829a3c177ab
Simon Fladen Lie <simonfl@uia.no>
2021-03-21 10:58:58 +0100

    Added multiple fragments, activitys and general layout design

---
+++ Inventory/Inventory/app/src/main/java/com/example/inventory/DefaultPage.kt
@@ -0,0 +1,47 @@
+package com.example.inventory
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.inventory.databinding.FragmentDefaultPageLayoutBinding
+
+/**
+ * A simple fragment subclass as the main(default) destination in the navigation.
+ * TODO: Change name of the activities to something more suitable
+ * TODO: Add some commenting
+ */
+class DefaultPage : Fragment() {
+
+    private var _binding: FragmentDefaultPageLayoutBinding? = null
+
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentDefaultPageLayoutBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
